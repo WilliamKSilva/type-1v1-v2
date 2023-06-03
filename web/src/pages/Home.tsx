0b3cc5b9d93631fd6d307249c292e4c8d5eb6f2e
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-06-03 18:02:12 -0300

    feat: start the Home page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import { Header } from "../components/Header"
+import './Home.scss'
 
 
 export const Home = () => {
     return (
-        <div className="main-container">
-            <p>Hello Type-1v1!</p>
+        <div>
+            <Header />
+            <div className="content">
+                <section className="description">
+                    <strong className="description-title">
+                        Desafie seus amigos e teste quem e o digitador mais rapido! 
+                    </strong>
+                    <p className="description-content">
+                        Escolha entre a corrida rapida e a corrida regular.
+                        Os dois modos de jogo foram feito especialmente para
+                        que a corrida seja simples e direto ao ponto! ;D
+                    </p>
+                </section>
+            </div>
         </div>
     )
 }
