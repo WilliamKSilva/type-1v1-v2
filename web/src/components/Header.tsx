cfa333b08ac184a3349c5fadbbd1144460d46a17
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-06-03 18:01:59 -0300

    feat: add Header component

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.scss'
+
+export const Header = () => {
+    return (
+        <header>
+            <div className="header-content">
+                <strong>Type-1v1</strong>
+                <button>teste</button>
+            </div>
+        </header>
+    )
+}
