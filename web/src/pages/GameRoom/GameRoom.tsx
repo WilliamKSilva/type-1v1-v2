3aaff8b9faa1fde295cef3e698ce4c833b476a23
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-06-08 17:03:58 -0300

    feat: add the first version of the GameRoom page

--- web/src/pages/GameRoom/GameRoom.tsx
+++ web/src/pages/GameRoom/GameRoom.tsx
@@ -1,7 +1,22 @@
+import "./GameRoom.scss";
+
 export default function GameRoom() {
   return (
-    <div>
-      <p>teste</p>
+    <div className="content">
+      <div className="player">
+        <strong className="name">William</strong>
+        <div className="text-container">
+          <p className="text">teste</p>
+        </div>
+        <input placeholder="Digite aqui..." />
+      </div>
+      <div className="player">
+        <strong className="name">Teste</strong>
+        <div className="text-container">
+          <p className="text">teste</p>
+        </div>
+        <input placeholder="Digite aqui..." />
+      </div>
     </div>
   );
 }
